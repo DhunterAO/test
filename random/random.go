53ef96eba4c4f81bac4c9bdfe094b4f5406892fc
Hunter AO <DhunterAO@users.noreply.github.com>
2020-08-04 18:28:53 +0800

    add annotations in random

--- random/random.go
+++ random/random.go
@@ -7,12 +7,14 @@ import (
 
 var ErrRandRange = errors.New("the range of RandIntIn should be larger than 0")
 
+// Generate random integer
 func RandInt() int {
 	return rand.Int()
 }
 
+// Generate random integer in range [l, r), return ErrRandRange if l is large or equal than r
 func RandIntIn(l, r int) (int, error) {
-	if l == r {
+	if l >= r {
 		return 0, ErrRandRange
 	}
 	return rand.Intn(r-l) + l, nil
