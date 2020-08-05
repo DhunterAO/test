2accc109990a8dfd9017c6679887f6944a2afc7a
Hunter AO <DhunterAO@users.noreply.github.com>
2020-08-05 12:02:36 +0800

    test validation

--- main_test.go
+++ main_test.go
@@ -2,6 +2,7 @@ package main
 
 import "testing"
 
+// test function for GenString, check the output
 func TestGenString(t *testing.T) {
 	if genStr := GenString(); genStr != "hello world" {
 		t.Error("The generated string should be \"hello word\", got ", genStr)
